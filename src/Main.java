154baf139dbfd100f20e6bf1124217312ed95a01
Daniela2319 <frankvt04@gmail.com>
2022-07-31 14:00:55 -0300

    final

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
+import br.com.dio.desafio.dominio.Bootcamp;
 import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Dev;
 import br.com.dio.desafio.dominio.Métorias;
 
 import java.time.LocalDate;
@@ -18,16 +20,54 @@ public class Main {
         curso2.setDescricao("descricao do curso jS");
         curso2.setCargaHoraria(4);
 
-        System.out.println(curso1);
-        System.out.println(curso2);
-
         Métorias metoria = new Métorias();
 
         metoria.setTitulo("Metoria java");
         metoria.setDescricao("Metoria descricao java");
         metoria.setData(LocalDate.now());
 
-        System.out.println(metoria);
+        /*System.out.println(curso1);
+        System.out.println(curso2);
+        System.out.println(metoria);*/
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descricao Bootcamp Java Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(metoria);
+
+        Dev devDaniela = new Dev();
+        devDaniela.setNome("Daniela");
+        devDaniela.inscreverBootcamp(bootcamp);
+        System.out.println("Conteudo Inscrito Daniela" + devDaniela.getConteudoInstrito());
+        devDaniela.progredir();
+        devDaniela.progredir();
+        System.out.println("-");
+        System.out.println("Conteudo Inscrito Daniela" + devDaniela.getConteudoInstrito());
+        System.out.println("Conteudo Concluidos Daniela" + devDaniela.getConteudoConcluido());
+        System.out.println("XP:  " + devDaniela.calcularTotalXp());
+
+
+        System.out.println("--------");
+
+        Dev devFrank = new Dev();
+        devFrank.setNome("Frank");
+        devFrank.inscreverBootcamp(bootcamp);
+        System.out.println("Conteudo Inscrito Frank" + devFrank.getConteudoInstrito());
+        devFrank.progredir();
+        devFrank.progredir();
+        devFrank.progredir();
+        System.out.println("-");
+        System.out.println("Conteudo Inscrito Frank" + devFrank.getConteudoInstrito());
+        System.out.println("Conteudo Concluidos Frank" + devFrank.getConteudoConcluido());
+        System.out.println("XP:  " + devFrank.calcularTotalXp());
+
+
+
+
+
+
 
     }
 }
